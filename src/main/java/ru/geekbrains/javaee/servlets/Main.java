5f25c2f2afe709149d58ecc8f533421aaba21add
msLapkin <java12102017@gmail.com>
2018-12-27 22:51:40 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/javaee/servlets/Main.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.javaee.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet(urlPatterns = {"/main"})
+public class Main extends HttpServlet{
+
+}
